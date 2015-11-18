3c53860b59f8edc9c98ed3bea4533eaa2fbcf3bc
Ling Lu <jojoucsd@gmail.com>
2015-11-18 14:07:30 -0800

    routes of log in crate is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ActiveRecord::Base
+	has_secure_password
+	validates :email, uniqueness: true
+	validates_length_of :first_name, :allow_blank => false
+	validates_length_of :last_name, :allow_blank => false
+
+	has_many :compliments
+	has_one :location
+
+	user = User.new
+	user.valid? 
+	user.errors[:email]
+	user.errors.full_messages
+
+	user.errors.clear
+	user.errors.empty? 
+ # def a_method_used_for_validation_purposes
+ #     errors.add(:email, "Email is already taken!")
+ # end
+
+ def self.confirm(params)
+ 	@user = User.find_by({email: params[:email]})
+ 	@user.try(:authenticate, params[:password])
+ end
 end
